2ec2c0191e76d8938a917650f9cb1fbeac9d26ab
Branden Rolston <branden@mesosphere.com>
2018-05-23 00:24:19 -0700

    Add runner from dcos-diagnostics
    Retrieved from
    https://github.com/dcos/dcos-diagnostics/commit/2487edc5cd43d63e7d13a91ef762d40b3ba044cc.

---
+++ config/config.go
@@ -0,0 +1,58 @@
+package config
+
+import (
+	"encoding/json"
+	"github.com/pkg/errors"
+)
+
+var (
+	// Version of dcos-diagnostics code.
+	Version = "0.4.0"
+
+	// APIVer is an API version.
+	APIVer = 1
+)
+
+// Config structure is a main config object
+type Config struct {
+	SystemdUnits []string `json:"-"`
+
+	// dcos-diagnostics flags
+	FlagCACertFile                 string `json:"ca-cert"`
+	FlagPull                       bool   `json:"pull"`
+	FlagVerbose                    bool   `json:"verbose"`
+	FlagPort                       int    `json:"port"`
+	FlagDisableUnixSocket          bool   `json:"no-unix-socket"`
+	FlagMasterPort                 int    `json:"master-port"`
+	FlagAgentPort                  int    `json:"agent-port"`
+	FlagPullInterval               int    `json:"pull-interval"`
+	FlagPullTimeoutSec             int    `json:"pull-timeout"`
+	FlagUpdateHealthReportInterval int    `json:"health-update-interval"`
+	FlagExhibitorClusterStatusURL  string `json:"exhibitor-ip"`
+	FlagForceTLS                   bool   `json:"force-tls"`
+	FlagDebug                      bool   `json:"debug"`
+	FlagRole                       string `json:"role"`
+	FlagIAMConfig                  string `json:"iam-config"`
+
+	// diagnostics job flags
+	FlagDiagnosticsBundleDir                     string `json:"diagnostics-bundle-dir"`
+	FlagDiagnosticsBundleEndpointsConfigFile     string `json:"endpoint-config"`
+	FlagDiagnosticsBundleUnitsLogsSinceString    string `json:"diagnostics-units-since"`
+	FlagDiagnosticsJobTimeoutMinutes             int    `json:"diagnostics-job-timeout"`
+	FlagDiagnosticsJobGetSingleURLTimeoutMinutes int    `json:"diagnostics-url-timeout"`
+	FlagCommandExecTimeoutSec                    int    `json:"command-exec-timeout"`
+}
+
+// LoadFromViper takes a map of flags with values and updates the config structure.
+func (c *Config) LoadFromViper(settings map[string]interface{}) error {
+	// TODO(mnaboka): use a map to struct library
+	body, err := json.Marshal(settings)
+	if err != nil {
+		return errors.Wrap(err, "unable to marshal config file")
+	}
+
+	if err := json.Unmarshal(body, c); err != nil {
+		return errors.Wrap(err, "unable to unmarshal config file")
+	}
+	return nil
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/dcos/dcos-diagnostics/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
