62b60dee09d4514756649f0e32d2b343c2d76750
Branden Rolston <branden@mesosphere.com>
2018-05-23 02:20:36 -0700

    Remove unused vars and flags

--- config/config.go
+++ config/config.go
@@ -8,39 +8,12 @@ import (
 var (
 	// Version of dcos-check-runner code.
 	Version = "0.4.0"
-
-	// APIVer is an API version.
-	APIVer = 1
 )
 
 // Config structure is a main config object
 type Config struct {
-	SystemdUnits []string `json:"-"`
-
-	// dcos-check-runner flags
-	FlagCACertFile                 string `json:"ca-cert"`
-	FlagPull                       bool   `json:"pull"`
-	FlagVerbose                    bool   `json:"verbose"`
-	FlagPort                       int    `json:"port"`
-	FlagDisableUnixSocket          bool   `json:"no-unix-socket"`
-	FlagMasterPort                 int    `json:"master-port"`
-	FlagAgentPort                  int    `json:"agent-port"`
-	FlagPullInterval               int    `json:"pull-interval"`
-	FlagPullTimeoutSec             int    `json:"pull-timeout"`
-	FlagUpdateHealthReportInterval int    `json:"health-update-interval"`
-	FlagExhibitorClusterStatusURL  string `json:"exhibitor-ip"`
-	FlagForceTLS                   bool   `json:"force-tls"`
-	FlagDebug                      bool   `json:"debug"`
-	FlagRole                       string `json:"role"`
-	FlagIAMConfig                  string `json:"iam-config"`
-
-	// diagnostics job flags
-	FlagDiagnosticsBundleDir                     string `json:"diagnostics-bundle-dir"`
-	FlagDiagnosticsBundleEndpointsConfigFile     string `json:"endpoint-config"`
-	FlagDiagnosticsBundleUnitsLogsSinceString    string `json:"diagnostics-units-since"`
-	FlagDiagnosticsJobTimeoutMinutes             int    `json:"diagnostics-job-timeout"`
-	FlagDiagnosticsJobGetSingleURLTimeoutMinutes int    `json:"diagnostics-url-timeout"`
-	FlagCommandExecTimeoutSec                    int    `json:"command-exec-timeout"`
+	FlagVerbose bool   `json:"verbose"`
+	FlagRole    string `json:"role"`
 }
 
 // LoadFromViper takes a map of flags with values and updates the config structure.
